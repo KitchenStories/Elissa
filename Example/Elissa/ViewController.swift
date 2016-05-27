678dfecd89dbcbdde086807e98453ae439041215
Kersten Broich <kersten.broich@gmail.com>
2016-05-27 16:34:10 +0200

    remove memory warning template

--- Example/Elissa/ViewController.swift
+++ Example/Elissa/ViewController.swift
@@ -17,12 +17,6 @@ class ViewController: UIViewController {
         
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
     @IBAction func someActionTrigger(sender: UIButton) {
         
