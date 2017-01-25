7e267ee36930d0fa900ed6ef734cbfe6b3730881
Justus Kandzi <justus.kandzi@gmail.com>
2017-01-25 12:03:02 +0100

    add carthage support

---
+++ Elissa/Elissa.h
@@ -0,0 +1,19 @@
+//
+//  Elissa.h
+//  Elissa
+//
+//  Created by Justus Kandzi on 25/01/2017.
+//  Copyright © 2017 AJNS New Media GMBH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Elissa.
+FOUNDATION_EXPORT double ElissaVersionNumber;
+
+//! Project version string for Elissa.
+FOUNDATION_EXPORT const unsigned char ElissaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Elissa/PublicHeader.h>
+
+
